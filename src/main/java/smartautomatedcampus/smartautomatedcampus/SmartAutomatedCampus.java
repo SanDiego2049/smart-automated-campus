38d8d92674a2eb1654b8791197aa3fa79dbe5284
Oreoluwa Alaba <alabaoreoluwa@gmail.com>
2026-02-28 09:46:31 +0000

    My first push to github

---
+++ src/main/java/smartautomatedcampus/smartautomatedcampus/SmartAutomatedCampus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package smartautomatedcampus.smartautomatedcampus;
+
+/**
+ *
+ * @author Penelope
+ */
+public class SmartAutomatedCampus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
